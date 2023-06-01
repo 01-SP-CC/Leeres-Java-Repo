ced960a8acbf99855ea2e85fc3319460a86e1fbd
Sven <132389521+sven-pke@users.noreply.github.com>
2023-06-01 16:08:31 +0200

    output wrapper

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,5 +5,10 @@ public class App {
     public static void main(String[] args) {
      
     }
+ 
     
+        private static void output(String outStr) {
+        System.out.println(outStr);
+    }
+
 }
